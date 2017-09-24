d0111a8f2f4ad3acdc896a3a3804511069930ce7
deniskyashif <denis.kyashif@gmail.com>
2017-09-24 23:46:05 +0300

    created typescript demos

--- demos/ts/type-checking.ts
+++
@@ -1,3 +0,0 @@
-function sum(x: number, y: number) {
-    return x + y;
-}
---
+++ demos/typescript/complete/esnext.ts
@@ -0,0 +1,46 @@
+// Template Strings
+const city = 'Paris';
+console.log(`Welcome to ${city}! Enjoy your stay.`)
+
+// Object Destructiong
+const obj = { a: 1, b: true, c: 'c' };
+const { a, b } = obj;
+console.log(a, b); // 1, true
+
+// Array Destructing
+const [x, y, ...rest] = [10, 20, 30, 40, 50];
+console.log(x, y, rest); // 10, 20, [30, 40, 50]
+
+// Default Values
+function logMessage(message: string = 'Hello World') {
+    console.log(message);
+}
+
+logMessage('TypeScript Rocks!'); // 'TypeScript Rocks!'
+logMessage(); // 'Hello World'
+
+// Arrow Functions
+const isEven = (n: number): boolean => {
+    return n % 2 === 0;
+};
+
+console.log(isEven(2)); // true
+
+const squares = [1, 2, 3, 4, 5].map(x => x * x);
+console.log(squares); // [1, 4, 9, 16, 25]
+
+// async, await
+function sleep(timeout: number) {
+    return new Promise(resolve => setTimeout(() => {
+        console.log(`Slept for ${timeout / 1000} seconds.`);
+        resolve();
+    }, timeout));
+}
+
+async function asyncFn() {
+    await sleep(2000);
+    console.log('Should print after setTimeout fn call.');
+}
+
+asyncFn();
+
