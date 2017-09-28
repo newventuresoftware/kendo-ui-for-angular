5d5445bf07e23c29e0a8c7d3a880d5028628ac9b
deniskyashif <denis.kyashif@gmail.com>
2017-09-29 00:16:09 +0300

    initial setup... - installed kendo grid module - installed kendo default theme - added a static movie dataset

--- demos/angular/src/app/app.module.ts
+++ demos/angular/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+    declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule, GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
