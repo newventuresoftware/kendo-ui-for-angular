a4fb6abd1e9e5a27fbffdb517bb3b03096d922bf
deniskyashif <denis.kyashif@gmail.com>
2017-09-30 11:44:29 +0300

    implemented demo functionality for ng-starter

--- demos/ng-starter/src/app/app.component.ts
+++ demos/ng-starter/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    message = 'Angular Rocks!';
+    color = '';
+    colors: string[] = [ 'default', 'red', 'green', 'blue' ];
+
+    onColorChange(color: string) {
+        this.color = color;
+    }
+
+    onAddColorClick(color) {
+        console.log(color);
+        this.colors.push(color);
+    }
 }
